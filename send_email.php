5b65e9fd3c7d2e2478bdf68082afb0af1e40cfb6
Anchit <53094342+AnchitGupta22@users.noreply.github.com>
2024-04-10 15:19:28 +0200

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,17 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $fullname = $_POST['fullname'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    
-    $to = "gupta.anchit.99@gmail.com";
-    $subject = "New Message from Website";
-    $body = "From: $fullname\nEmail: $email\nMessage:\n$message";
-    
-    if (mail($to, $subject, $body)) {
-        echo "Message sent successfully!";
-    } else {
-        echo "Oops! Something went wrong.";
-    }
-}
-?>
+
